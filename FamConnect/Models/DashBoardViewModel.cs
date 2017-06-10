d3169475270aa83b559ac32995b8b5dade0e481c
russpetrus <russpetrus@gmail.com>
2017-06-10 17:03:06 -0400

    added DashBoardViewModel

---
+++ FamConnect/Models/DashBoardViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity;
+
+namespace FamConnect.Models
+{
+    public class DashBoardViewModel
+    {
+        public ApplicationUser currentUser { get; set; }
+        public ICollection <FamilyMember> FamilyMembers { get; set; }
+        public ICollection <Milestone> Milestones { get; set; }
+        public ICollection <ConnectionAction> ConnectionActions { get; set; } 
+    }
+}
